eaa0fb76ab7556d432ee6957991f081793ff4e2c
mnsmns48 <getzboy@list.ru>
2023-11-05 16:21:08 +0300

    !

--- page_views.py
+++ page_views.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.responses import HTMLResponse
 
 from core.crud import get_directory
 from core.engine import pg_engine
@@ -9,7 +10,7 @@ pages_router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 
-@pages_router.get("/")
+@pages_router.get("/", response_class=HTMLResponse)
 async def get_page(
     request: Request,
     session: AsyncSession = Depends(dependency=pg_engine.scoped_session_dependency),
