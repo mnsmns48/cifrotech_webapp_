798b5447a35eb8ff55ba6acbb202b096bb884503
mnsmns48 <getzboy@list.ru>
2025-03-27 22:54:34 +0300

    add render_book form

--- aservice88/aservice_views.py
+++ aservice88/aservice_views.py
@@ -1,6 +1,10 @@
+from io import BytesIO
+
 from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
-from starlette.responses import HTMLResponse
+from num2words import num2words
+from starlette.responses import HTMLResponse, StreamingResponse
+from xlsxtpl.writerx import BookWriter
 
 from aservice88.schemas import take_form_result
 from cfg import BASE_DIR
@@ -14,7 +18,21 @@ async def bill_editor(request: Request):
     context = {"request": request}
     return templates.TemplateResponse(name="cash_receipt_editor.html", context=context)
 
+
 @aservice88.post("/submit", response_class=HTMLResponse)
-async def submit_link(request: Request, form = Depends(take_form_result)):
-    print(form)
-    return templates.TemplateResponse(name="cash_receipt_editor.html", context = {"request": request})
+async def submit_link(request: Request, form=Depends(take_form_result)):
+    xlsx_template = BASE_DIR / "aservice88/tmplts/E1.xlsx"
+    with open(xlsx_template, "rb") as template_file:
+        file_stream = BytesIO(template_file.read())
+        writer = BookWriter(file_stream)
+    total: int = form['receipt_qty'] * form['receipt_price']
+    form.update({'total_by_words': num2words(int(total), lang='ru')})
+    writer.render_book([form])
+    result_stream = BytesIO()
+    writer.save(result_stream)
+    result_stream.seek(0)
+    filename = form['receipt_product'].replace(' ', '_')
+    response = StreamingResponse(result_stream,
+                                 media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    response.headers["Content-Disposition"] = f"attachment; filename=receipt-{filename[:25]}.xlsx"
+    return response
