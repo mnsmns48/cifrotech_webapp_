c3b6cdb6f6e273be7f2b662a4820a128a83db225
r_mns <getzboy@list.ru>
2025-06-06 15:37:03 +0300

    add cost_value_update

--- main.py
+++ main.py
@@ -43,5 +43,5 @@ app.include_router(general_router)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.DEBUG)
     uvicorn.run("main:app", host='0.0.0.0', port=5000)
