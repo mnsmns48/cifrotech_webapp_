11d0dd593a7a8a985eb8a0fcf59ca6ad4739852c
mnsmns48 <getzboy@list.ru>
2025-10-11 23:59:01 +0300

    fix get_hubstock_items

--- bot/admin/keyboards_admin.py
+++ bot/admin/keyboards_admin.py
@@ -1,8 +1,11 @@
 from aiogram.types import KeyboardButton, WebAppInfo, ReplyKeyboardMarkup
 
+from config import settings
+
 admin_basic_ = [
     [KeyboardButton(text='Продажи сегодня')],
     [KeyboardButton(text='W-APP', web_app=WebAppInfo(url='https://24cifrotech.ru'))],
+    [KeyboardButton(text='TEST', web_app=WebAppInfo(url=f"https://24cifrotech.ru/webapp"))],
 ]
 
 admin_basic_kb = ReplyKeyboardMarkup(resize_keyboard=True, keyboard=admin_basic_)
