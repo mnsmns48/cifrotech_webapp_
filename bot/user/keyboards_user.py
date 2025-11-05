65af6bc1a6ccfe1b74fbb2fbd079d353bb20e716
mnsmns48 <getzboy@list.ru>
2025-11-05 21:55:47 +0300

    some fix

--- bot/user/keyboards_user.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
-
-main_menu_ = [
-    [KeyboardButton(text='Актуально в Цифрохаб')],
-    [KeyboardButton(text='Наличие в Цифротех', web_app=WebAppInfo(url='https://24cifrotech.ru'))],
-]
-
-user_kb = ReplyKeyboardMarkup(resize_keyboard=True, is_persistent=True, keyboard=main_menu_)
