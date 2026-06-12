4a1eb2bc96bfdb86573dc9caa25f7ec632ca5992
r_mns <getzboy@list.ru>
2026-06-12 16:44:54 +0300

    fix description_router

--- api_common/routers/__init__.py
+++ api_common/routers/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+from api_common.routers.description_router import description_router
 from api_common.routers.progress import progress_router
 
 general_router = APIRouter(tags=['General'])
 
 general_router.include_router(progress_router)
+general_router.include_router(description_router)
